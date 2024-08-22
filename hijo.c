9ca9ea222aad4912c65f4bcc3cf3fc0024312bfa
ariel <ariel@gmail.com>
2024-08-22 17:33:48 +0000

    $ arreglado

--- hijo.c
+++ hijo.c
@@ -7,19 +7,21 @@
  */
 void comando(char **token, char **env)
 {
-	pid_t pid = fork();
+	pid_t pid;
 	int status = 0;
 	struct stat fileStat;
+
+	if (stat(token[0], &fileStat) == 0){
+		pid = fork();
 
 	if (pid < 0)
 		printf("error");
 	else if (pid == 0)
-	{
-		if (stat(token[0], &fileStat) == 0)
-			execve(token[0], token, env);
-		else
-			printf("Comando no encontrado");
-	}
+		execve(token[0], token, env);
 	else
 		wait(&status);
+}
+else {
+	printf("Comando no encontrado\n");
+}
 }
